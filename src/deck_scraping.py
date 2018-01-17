07d2d309c2e8bdfdbce0c6f45b6a47c4df1b872a
Ben Walzer <bwalzer@me.com>
2018-01-17 13:32:20 -0800

    Modified src/deck_scraping.py to do the entire scraping process: scraping deck lists, making user_card_count pairs and uploading them to the database. The function upload_user_card_counts() is not implemented yet.

--- src/deck_scraping.py
+++ src/deck_scraping.py
@@ -5,8 +5,128 @@ import random
 import time
 import boto3
 from bs4 import BeautifulSoup
+import psycopg2
+import os
+from os import listdir # used for looping through all the files in a directory
 
 s3 = boto3.client('s3')
+
+dbname = os.environ['CAPSTONE_DB_DBNAME']
+host = os.environ['CAPSTONE_DB_HOST']
+username = os.environ['CAPSTONE_DB_USERNAME']
+
+conn = psycopg2.connect('dbname={} host={} user={}'.format(dbname, host, username))
+cursor = conn.cursor()
+
+def format_deck(raw_deck_list):
+    """
+    Takes a raw deck list and returns a nicely formatted deck list.
+
+    INPUT:
+        - raw_deck_list: String, unformatted deck list read directly from a file
+
+    OUTPUT:
+        - deck_list: list of strings, a formatted deck list ready for processing
+    """
+    deck_list = []
+    for row in raw_deck_list.split('\r\n'):
+        print(row)
+        if row.startswith('S'):
+            break
+        deck_list.append(row)
+
+    return deck_list
+
+def parse_card_string(card_string):
+    """
+    Parses a single row of a deck list.
+
+    INPUT:
+        - card_string: a string of a row of the deck list
+
+    OUTPUT:
+        - card_name: a string of the name of the card
+        - card_count: an int of the number of this card in the deck
+    """
+    card_count = int(card_string[0])
+    card_name = card_string[2:]
+
+    return card_name, card_count
+
+def make_user_card_counts(deck_id, deck_list):
+    """
+    Takes a deck_id and deck_list and returns user-card-count tuples.
+
+    INPUT:
+        - deck_id: the unique identifier for the deck
+        - deck_list: the deck list read from a file, formatted
+    OUTPUT:
+        - user_card_count: a tuple containing the deck_id, card_name and
+                           card_count for each card in the deck.
+    """
+
+    user_card_count = []
+    for card_string in deck_list:
+        # hit the sideboard, done
+        if card_string[0] == 'S':
+            break
+        card_name, card_count = parse_card_string(card_string)
+
+        user_card_count.append((deck_id, card_name, card_count))
+
+    return user_card_count
+
+def read_deck_list(filepath):
+    """
+    Takes a filepath and reads the contents of the raw deck list stored there.
+
+    INPUT:
+        - filepath: string, the full path of the requested file,
+                    i.e, 'data/raw_deck_lists/deck_list_371829'
+
+    OUTPUT:
+        - raw_deck_list: string, the un-formatted deck list read from the file
+    """
+    s3 = boto3.client('s3')
+    bucketname = 'mtg-capstone'
+
+    response = s3.get_object(Bucket=bucketname, Key=filepath)
+    raw_deck_list = response['Body'].read().decode()
+
+    return raw_deck_list
+
+def add_item_db(user_card_count):
+    """
+    Adds a user_card_count item to the Postgres database.
+
+    INPUT:
+        - user_card_count: a tuple to be inserted into the db.
+
+    OUTPUT:
+        NONE
+
+    """
+
+    query = """INSERT INTO user_card_counts (user_id, card_name, card_count)
+               VALUES ({user_id}, '{card_name}', {card_count})
+            """.format(user_id=user_card_count[0], card_name=user_card_count[1],
+                       card_count=user_card_count[2])
+
+    cursor.execute(query)
+
+    conn.commit()
+
+def get_deck_id(filepath):
+    """
+    Gets the deck id from the end of the filepath string.
+    """
+
+    return int(re.search('deck_list_(\d+).txt', deck_file).group(1))
+
+
+################################################################################
+############## Above functions from make_user_card_counts.py ###################
+################################################################################
 
 def get_event_ids(front_page, verbose=False):
     """
@@ -188,6 +308,23 @@ def get_deck_lists(event_ids, verbose=False):
 
     return deck_lists
 
+def save_decklists(deck_id, deck_list, verbose=False):
+    """
+    Saves a deck list as a text file in my s3 bucket.
+
+    INPUT:
+        - deck_id: the unique id for the deck list
+        - deck_list: the deck list to be saved in the s3 bucket.
+
+    OUTPUT:
+        NONE
+    """
+
+    bucketname = 'mtg-capstone'
+    filename = 'data/raw_deck_lists/deck_list_{}.txt'.format(deck_id)
+
+    s3.put_object(Bucket=bucketname, Key=filename, Body=deck_list)
+
 def scrape_decklists(scraped_events, front_pages=[0], verbose=False):
     """
     Version 2. Scrapes mtgtop8.com for modern deck lists.
@@ -228,24 +365,40 @@ def scrape_decklists(scraped_events, front_pages=[0], verbose=False):
 
     return scraped_events
 
-def save_decklists(deck_id, deck_list, verbose=False):
-    """
-    Saves a deck list as a text file in my s3 bucket.
+def scrape_decklists_2(scraped_events, front_pages=[0], verbose=False):
+    '''
 
-    INPUT:
-        - deck_id: the unique id for the deck list
-        - deck_list: the deck list to be saved in the s3 bucket.
+    Front page request
+        get event ids
+        for event ids:
+            event request
+            get deck ids
+            for deck_ids:
+                deck request
+                make user-card-counts
+                upload to db
+    '''
 
-    OUTPUT:
-        NONE
-    """
+    raw_front_page = modern_front_page_request(page_number=page_number, verbose=verbose)
+    front_page = BeautifulSoup(raw_front_page.text, 'html.parser')
 
-    bucketname = 'mtg-capstone'
-    filename = 'data/raw_deck_lists/deck_list_{}.txt'.format(deck_id)
+    event_ids = get_event_ids(front_page, verbose=verbose)
 
-    s3.put_object(Bucket=bucketname, Key=filename, Body=deck_list)
+    for event_id in event_ids:
+        if event_id in scraped_events:
+            if verbose: print('event id {} has already been scraped'.format(event_id))
+            continue
+        raw_event_page = event_request(event_id, verbose=verbose)
+        event_page = BeautifulSoup(raw_event_page.text, 'html.parser')
 
+        deck_ids = get_deck_ids(event_page, verbose=verbose)
 
+        for deck_id in deck_ids:
+            raw_deck_list = deck_request(deck_id, verbose=verbose)
+            deck_list = format_deck(raw_deck_list)
+            user_card_counts = make_user_card_counts(deck_id, deck_list)
+
+            upload_user_card_counts(user_card_counts)
 
 if __name__ == '__main__':
     response = s3.get_object(Bucket='mtg-capstone', Key='data/scraped_events.json')
