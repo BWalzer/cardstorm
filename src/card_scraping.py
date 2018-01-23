e53f6cd7fd4d1e2289c55476bb173d62d201c7f9
Ben Walzer <bwalzer@me.com>
2018-01-23 11:38:08 -0800

    Updated card_scraping.py. Successfully deals with all potential modern card layouts, aand adds layouts to cards db!

--- src/card_scraping.py
+++ src/card_scraping.py
@@ -3,6 +3,170 @@ import json
 import psycopg2
 import os
 
+def format_card_2(card):
+    '''
+    Formats cards in a db friendly manner. If the card is multifaced, the first face is used for missing fields
+
+    INPUT:
+        - card: dictionary, magic card from scryfall.com
+
+    OUTPUT:
+        - formatted_card: list, attributes of each card in the following order:
+                            [name, cmc, type_line, oracle_text, mana_cost, power, toughness, colors,
+                            color_identity, legalities, set_code, set_name, collector_number, scryfall_id]
+    '''
+    formatted_card = []
+    layout = card['layout']
+    keys = card.keys()
+    if layout == 'normal' or layout == 'leveler':
+        # normal layout, i.e, Lightning Bolt
+        formatted_card.append(card['name'])
+        formatted_card.append(card['cmc'])
+        formatted_card.append(card['type_line'])
+
+        if 'oracle_text' in keys:
+            formatted_card.append(card['oracle_text'])
+        else:
+            formatted_card.append(None)
+
+        formatted_card.append(card['mana_cost'])
+
+        if 'power' in keys:
+            formatted_card.append(card['power'])
+        else:
+            formatted_card.append(None)
+
+        if 'toughness' in keys:
+            formatted_card.append(card['toughness'])
+        else:
+            formatted_card.append(None)
+
+        formatted_card.append(card['colors'])
+        formatted_card.append(card['color_identity'])
+
+        legalities = [k for k, v in card['legalities'].items() if v == 'legal']
+        formatted_card.append(legalities)
+        formatted_card.append(card['set'])
+        formatted_card.append(card['set_name'])
+        formatted_card.append(card['collector_number'])
+        formatted_card.append(card['id'])
+        formatted_card.append(card['layout'])
+
+    elif layout == 'split':
+        # split layout, i.e, (Dusk // Dawn), (Boom // Bust)
+        side_a = card['card_faces'][0]
+        side_b = card['card_faces'][1]
+
+        formatted_card.append(card['name'])
+        formatted_card.append(card['cmc'])
+        formatted_card.append(' // '.join([side_a['type_line'], side_b['type_line']]))
+        formatted_card.append(' // '.join([side_a['oracle_text'], side_b['oracle_text']]))
+        formatted_card.append(card['mana_cost'])
+        formatted_card.append(None)
+        formatted_card.append(None)
+        formatted_card.append(card['colors'])
+        formatted_card.append(card['color_identity'])
+        legalities = [k for k, v in card['legalities'].items() if v == 'legal']
+        formatted_card.append(legalities)
+        formatted_card.append(card['set'])
+        formatted_card.append(card['set_name'])
+        formatted_card.append(card['collector_number'])
+        formatted_card.append(card['id'])
+        formatted_card.append(card['layout'])
+
+    elif layout == 'flip':
+        # flip layout, i.e, Akki Lavarunner
+        side_a = card['card_faces'][0]
+        side_b = card['card_faces'][1]
+
+        formatted_card.append(side_a['name'])
+        formatted_card.append(card['cmc'])
+        formatted_card.append(' // '.join([side_a['type_line'], side_b['type_line']]))
+        formatted_card.append(' // '.join([side_a['oracle_text'], side_b['oracle_text']]))
+        formatted_card.append(card['mana_cost'])
+        formatted_card.append(side_a['power'])
+        formatted_card.append(side_a['toughness'])
+        formatted_card.append(card['colors'])
+        formatted_card.append(card['color_identity'])
+        legalities = [k for k, v in card['legalities'].items() if v == 'legal']
+        formatted_card.append(legalities)
+        formatted_card.append(card['set'])
+        formatted_card.append(card['set_name'])
+        formatted_card.append(card['collector_number'])
+        formatted_card.append(card['id'])
+        formatted_card.append(card['layout'])
+
+    elif layout == 'transform':
+        # transform layout, i.e, Delver of Secrets
+        side_a = card['card_faces'][0]
+        side_b = card['card_faces'][1]
+
+        side_a_keys = side_a.keys()
+        side_b_keys = side_b.keys()
+
+        formatted_card.append(side_a['name'])
+        formatted_card.append(card['cmc'])
+        formatted_card.append(' // '.join([side_a['type_line'], side_b['type_line']]))
+        if 'oracle_text' in side_a_keys: a_oracle_text = side_a['oracle_text']
+        else: a_oracle_text = ''
+        if 'oracle_text' in side_b_keys: b_oracle_text = side_b['oracle_text']
+        else: b_oracle_text = ''
+        formatted_card.append(' // '.join([a_oracle_text, b_oracle_text]))
+        formatted_card.append(side_a['mana_cost'])
+        if 'power' in side_a_keys:
+            formatted_card.append(side_a['power'])
+        else:
+            formatted_card.append(None)
+        if 'toughness' in side_a_keys:
+            formatted_card.append(side_a['toughness'])
+        else:
+            formatted_card.append(None)
+        formatted_card.append(side_a['colors'] + side_b['colors'])
+        formatted_card.append(card['color_identity'])
+        legalities = [k for k, v in card['legalities'].items() if v == 'legal']
+        formatted_card.append(legalities)
+        formatted_card.append(card['set'])
+        formatted_card.append(card['set_name'])
+        formatted_card.append(card['collector_number'])
+        formatted_card.append(card['id'])
+        formatted_card.append(card['layout'])
+
+    elif layout == 'meld':
+        # meld layout, i.e, Bruna, the Fading Light
+        # this layout turns out to be pretty much the same as a normal card
+        formatted_card.append(card['name'])
+        formatted_card.append(card['cmc'])
+        formatted_card.append(card['type_line'])
+
+        if 'oracle_text' in keys:
+            formatted_card.append(card['oracle_text'])
+        else:
+            formatted_card.append(None)
+
+        formatted_card.append(card['mana_cost'])
+
+        if 'power' in keys:
+            formatted_card.append(card['power'])
+        else:
+            formatted_card.append(None)
+
+        if 'toughness' in keys:
+            formatted_card.append(card['toughness'])
+        else:
+            formatted_card.append(None)
+
+        formatted_card.append(card['colors'])
+        formatted_card.append(card['color_identity'])
+
+        legalities = [k for k, v in card['legalities'].items() if v == 'legal']
+        formatted_card.append(legalities)
+        formatted_card.append(card['set'])
+        formatted_card.append(card['set_name'])
+        formatted_card.append(card['collector_number'])
+        formatted_card.append(card['id'])
+        formatted_card.append(card['layout'])
+
+    return formatted_card
 
 def format_card(card):
     '''
@@ -99,7 +263,7 @@ def upload_card(card, cursor, verbose=False):
     template = ', '.join(['%s'] * len(card))
 
     query = '''INSERT INTO cards (name, cmc, type_line, oracle_text, mana_cost, power, toughness, colors,
-                              color_identity, legalities, set_id, set_name, collector_number, scryfall_id)
+                              color_identity, legalities, set_id, set_name, collector_number, scryfall_id, layout)
            VALUES ({})'''.format(template)
 
     try:
@@ -128,9 +292,8 @@ def scrape_modern_cards(verbose=False):
 
         if verbose: print('processing cards')
         for i, raw_card in enumerate(json_response['data']):
-            if verbose: print('{}'.format(i), end=', ')
-            card = format_card(raw_card)
-            if verbose: print('{}'.format(card[0]), end=', ')
+            if verbose: print('{}, "{}"'.format(i, raw_card['name']))
+            card = format_card_2(raw_card)
             status = upload_card(card, cursor, verbose=verbose)
 
             if status:
