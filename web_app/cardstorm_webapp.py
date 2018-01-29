d501459ddc81a22106bc3bb2e8a23c23ac792673
Ben Walzer <bwalzer@me.com>
2018-01-28 16:58:03 -0800

    started making the web app. template found from w3layouts

---
+++ web_app/cardstorm_webapp.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/input_card', methods = ['POST'])
+def submit_decklist():
+    # get the deck_list
+    request.form.get('deck_list')
+
+    # get the user choices
+    white_cards = request.form.get('white_cards')
+    blue_cards = request.form.get('blue_cards')
+    black_cards = request.form.get('black_cards')
+    red_cards = request.form.get('red_cards')
+    green_cards = request.form.get('green_cards')
+    land_cards = request.form.get('land_cards')
+
+    print(type(white_cards))
+    print(white_cards)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
