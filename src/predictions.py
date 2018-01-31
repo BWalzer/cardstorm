fa78fc1f60759ee54fb453bcaf093795253e0f0d
Ben Walzer <bwalzer@me.com>
2018-01-31 11:37:25 -0800

    Updated website. Reformatted predictions.py to an object, added filter options to predictions.py

--- src/predictions.py
+++ src/predictions.py
@@ -1,166 +1,396 @@
 import psycopg2
 import os
-from deck_scraping import format_deck, ReflexiveDict, parse_card_string
+from deck_scraping import ReflexiveDict, parse_card_string
 import numpy as np
 
-dbname = os.environ['CAPSTONE_DB_DBNAME']
-host = os.environ['CAPSTONE_DB_HOST']
-username = os.environ['CAPSTONE_DB_USERNAME']
-password = os.environ['CAPSTONE_DB_PASSWORD']
+# dbname = os.environ['CAPSTONE_DB_DBNAME']
+# host = os.environ['CAPSTONE_DB_HOST']
+# username = os.environ['CAPSTONE_DB_USERNAME']
+# password = os.environ['CAPSTONE_DB_PASSWORD']
+#
+# conn = psycopg2.connect('dbname={} host={} user={} password={}'.format(dbname, host, username, password))
+# cursor = conn.cursor()
 
-conn = psycopg2.connect('dbname={} host={} user={} password={}'.format(dbname, host, username, password))
-cursor = conn.cursor()
+class CardRecommender:
 
-def deck_to_dict(deck_list, card_dict):
-    '''
-        Turns a raw deck_list into a dictionary with cardstorm ids as keys
-        and counts as values
+    def __init__(self):
+        self._connect_to_db()
+        self.feature_matrix = self._get_feature_matrix()
+        self.card_dict = ReflexiveDict()
+        self.all_cardstorm_ids = self.card_dict.get_cardstorm_ids()
 
-        INPUT:
-            - deck_list: string, plaintext deck list with \n between each card line
-            - card_dict: ReflexiveDict object, dictionary with all cards and
-                            cardstorm_ids pointing to each other
+    def _connect_to_db(self):
+        db_name = os.environ['CAPSTONE_DB_DBNAME']
+        db_host = os.environ['CAPSTONE_DB_HOST']
+        db_username = os.environ['CAPSTONE_DB_USERNAME']
+        db_password = os.environ['CAPSTONE_DB_PASSWORD']
 
-        OUTPUT:
-            - deck_dict: dictionary, dict with cardstorm ids as keys and lowercase
-                            card counts as values
-    '''
-    deck_dict = {}
-    for card_string in deck_list.split('\n'):
-        if card_string.startswith('S'):
-            break
-        if not card_string:
-            break
-        parse_status, card_name, card_count = parse_card_string(card_string)
-        try:
-            cardstorm_id = card_dict[card_name.lower()]
-        except KeyError:
-            print('card "{}" not found'.format(card_name))
-        deck_dict[cardstorm_id] = int(card_count)
+        self.conn = psycopg2.connect('dbname={} host={} user={} password={}'.format(db_name, db_host, db_username, db_password))
+        self.cursor = self.conn.cursor()
 
-    return deck_dict
+    def _get_feature_matrix(self):
+        '''
+        Gets the most recent feature matrix from the database and expands it.
+        '''
 
-def vectorize_deck(all_cardstorm_ids, deck_dict):
-    '''
-        Creates an (n x 1) vector, where n is the number of available cards
+        query = '''SELECT cardstorm_id, features
+                   FROM product_matrices
+                   WHERE date = (SELECT MAX(date) FROM product_matrices)
+                   ORDER BY cardstorm_id ASC'''
+        self.cursor.execute(query)
 
-        INPUT:
-            - all_cardstorm_ids: numpy array of all the available cardstorm_ids
-            - deck_dict: dict representation of user submitted deck list. keys
-                            are cardstorm_ids and values are card counts
-        OUTPUT:
-            - deck_vector: numpy array of shape (n x 1), where n is the number of
-                            cardstorm_ids in all_cardstorm_ids. Each entry corresponds
-                            to the corresponding cardstorm_id in all_cardstorm_ids,
-                            and it is either the count of the card in the deck or 0
-                            if it's not included in the deck.
-    '''
+        feature_matrix = []
 
-    deck_vector = []
+        for cardstorm_id, features in self.cursor.fetchall():
+            feature_matrix.append(features)
 
-    for cardstorm_id in all_cardstorm_ids:
-        if cardstorm_id in deck_dict:
-            deck_vector.append(deck_dict[cardstorm_id])
-        else:
-            deck_vector.append(0)
+        feature_matrix = np.array(feature_matrix)
 
-    deck_vector = np.array(deck_vector)
+        return feature_matrix
 
-    return deck_vector
+    def fit(self, raw_deck_list):
+        '''
+        Solves for the 'u' vector, given d and V.
 
-def get_feature_matrix():
-    '''
-        Pulls the most recent feature matrix from the databse and formats it properly
-
-        INPUT:
-            NONE
-        OUTPUT:
-            - feature_matrix: numpy array of size (n x k), where n is the number of
-                                cards and k is chosen in the modeling process
-    '''
-    global cursor
-    query = '''SELECT cardstorm_id, features
-               FROM product_matrices
-               WHERE date = (SELECT MAX(date) FROM product_matrices)
-               ORDER BY cardstorm_id ASC'''
-    cursor.execute(query)
-
-    feature_matrix = []
-
-    for cardstorm_id, features in cursor.fetchall():
-        feature_matrix.append(features)
-
-    feature_matrix = np.array(feature_matrix)
-
-    return feature_matrix
-
-def solve_system(feature_matrix, deck_vector):
-    '''
-        Gets the least squares solution to the problem.
-
-        INPUT:
-            - feature_matrix: numpy array of shape (n x k), where n is the number of
-                                cards and k is chosen in the modeling process
-            - deck_vector: numpy array of shape (n x 1), where n is the number of
-                            cards
-
-        OUTPUT:
-            - u_vector: numpy array, the least squares solution to the system
-                            deck_vector = (u_vector)*(feature_matrix)
-    '''
-
-    if len(deck_vector) > 0:
-        u_vector = np.linalg.lstsq(feature_matrix, deck_vector)[0]
-    else:
-        return np.array([])
-
-    return u_vector
-
-def top_n_recommendations(recommendations, deck_vector, all_cardstorm_ids, n=10):
-    '''
-        Gets the top n recommendations from the recreated deck list
-
-        INPUT:
-            - recommendations: numpy array, the recreated deck_vector with predictions filled in
-            - deck_vector: numpy array, the original deck_vector
-            - n: int, the number of recommendations desired. default 10
-
-        OUTPUT:
-            top_recommendations: numpy array, the top n recommendations in descending order
-    '''
-    top_recommendations = all_cardstorm_ids[np.argsort(recommendations - deck_vector)[:-n-1:-1]]
-
-    return top_recommendations
-
-def make_recommendations(raw_deck_list, num_top_recommendations=10):
-    '''
-
-        get all cardstorm ids
-        read user submitted deck file
         make deck dictionary
         make deck vector
-        get feature matrix from db
         solve for u
-        get recommendations
 
-    '''
-    card_dict = ReflexiveDict()
-    all_cardstorm_ids = card_dict.get_cardstorm_ids()
+        '''
+        deck_dict = self._deck_to_dict(raw_deck_list)
+        self.deck_vector = self._vectorize_deck(deck_dict)
 
-    deck_dict = deck_to_dict(raw_deck_list, card_dict)
+        # u vector from the equation d = u*V
+        u_vector = np.linalg.lstsq(self.feature_matrix, self.deck_vector)[0]
 
-    deck_vector = vectorize_deck(all_cardstorm_ids, deck_dict)
+        # recreated user deck list
+        self.d_vector = np.dot(u_vector, self.feature_matrix.T)
 
-    feature_matrix = get_feature_matrix()
+    def recommend(self, land_filter=False, white_filter=False, blue_filter=False, black_filter=False, red_filter=False, green_filter=False):
+        '''
+        Takes the dot product of u and V to get new ratings for the 'd' vector.
+        '''
+        recommendations = self.all_cardstorm_ids[np.argsort(self.d_vector - self.deck_vector)[::-1]]
 
-    u_vector = solve_system(feature_matrix, deck_vector)
+        if land_filter:
+            recommendations = self._filter_lands(recommendations)
+        if white_filter:
+            recommendations = self._filter_white(recommendations)
+        if blue_filter:
+            recommendations = self._filter_blue(recommendations)
+        if black_filter:
+            recommendations = self._filter_black(recommendations)
+        if red_filter:
+            recommendations = self._filter_red(recommendations)
+        if green_filter:
+            recommendations = self._filter_green(recommendations)
 
-    if len(u_vector) == 0: # something went wrong
-        print('deck vector was empty')
-        return
+        return recommendations
 
-    recommendations = np.dot(u_vector, feature_matrix.T)
+    def _filter_lands(self, recommendations):
+        '''
+        Takes the recommendations and remove all land cards.
+        '''
 
-    top_recommendations = top_n_recommendations(recommendations, deck_vector, all_cardstorm_ids, n=num_top_recommendations)
+        query = "SELECT cardstorm_id FROM cards WHERE type_line LIKE '%Land%' AND NOT type_line LIKE '%//%Land%'"
 
-    return top_recommendations
+        cursor.execute(query)
+
+        land_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _filter_white(self, recommendations):
+        '''
+        Takes the recommendations and remove all white cards.
+        '''
+
+        query = "SELECT cardstorm_id FROM cards WHERE 'R'=ANY(colors)"
+
+        cursor.execute(query)
+
+        white_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _filter_blue(self, recommendations):
+        '''
+        Takes the recommendations and remove all blue cards.
+        '''
+
+        query = "SELECT cardstorm_id FROM cards WHERE 'U'=ANY(colors)"
+
+        cursor.execute(query)
+
+        blue_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _filter_black(self, recommendations):
+        '''
+        Takes the recommendations and remove all black cards.
+        '''
+
+        query = "SELECT cardstorm_id FROM cards WHERE 'B'=ANY(colors)"
+
+        cursor.execute(query)
+
+        black_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _filter_red(self, recommendations):
+        '''
+        Takes the recommendations and remove all red cards.
+        '''
+
+        query = "SELECT cardstorm_id FROM cards WHERE 'R'=ANY(colors)"
+
+        cursor.execute(query)
+
+        red_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _filter_green(self, recommendations):
+        '''
+        Takes the recommendations and remove all green cards.
+        '''
+
+        query = "SELECT cardstorm_id FROM cards WHERE 'G'=ANY(colors)"
+
+        cursor.execute(query)
+
+        green_ids = {_[0] for _ in cursor.fetchall()}
+
+        filtered_recommendations = [cardstorm_id for cardstorm_id in recommendations if not cardstorm_id in land_ids]
+
+        return filtered_recommendations
+
+    def _vectorize_deck(self, deck_dict):
+        '''
+            Creates an (n x 1) vector, where n is the number of available cards
+
+            INPUT:
+                - deck_dict: dictionary, keys are cardstorm ids and values are card counts
+
+            OUTPUT:
+                - deck_vector: numpy array of shape (n x 1), where n is the number of
+                                cardstorm_ids in all_cardstorm_ids. Each entry corresponds
+                                to the corresponding cardstorm_id in all_cardstorm_ids,
+                                and it is either the count of the card in the deck or 0
+                                if it's not included in the deck.
+        '''
+
+        deck_vector = []
+
+        for cardstorm_id in self.all_cardstorm_ids:
+            if cardstorm_id in deck_dict:
+                deck_vector.append(deck_dict[cardstorm_id])
+            else:
+                deck_vector.append(0)
+
+        deck_vector = np.array(deck_vector)
+
+        return deck_vector
+
+    def _deck_to_dict(self, raw_deck_list):
+        '''
+        Turns a raw deck list into a dictionary, with cardstorm ids as keys and
+        card counts as values
+
+        INPUT:
+            - raw_deck_list: string, plaintext deck list with cardstrings
+                              separated by newlines
+        OUTPUT:
+            - deck_dict: dictionary, cardstorm ids as keys and card counts as values
+        '''
+
+        deck_dict = {}
+        for card_string in raw_deck_list.split('\n'):
+            if card_string.lower().startswith('sideboard'):
+                # we found the sideboard, ignore everything past this line
+                break
+            if not card_string:
+                # we found an empty line, ignore everything past this line
+                break
+            parse_status, card_name, card_count = parse_card_string(card_string)
+            try:
+                cardstorm_id = self.card_dict[card_name.lower()]
+            except KeyError:
+                print('card "{}" not found'.format(card_name))
+            deck_dict[cardstorm_id] = int(card_count)
+
+        return deck_dict
+
+
+################################################################################
+############################# OLD VERSIONS #####################################
+################################################################################
+
+
+# def deck_to_dict(deck_list, card_dict):
+#     '''
+#         Turns a raw deck_list into a dictionary with cardstorm ids as keys
+#         and counts as values
+#
+#         INPUT:
+#             - deck_list: string, plaintext deck list with \n between each card line
+#             - card_dict: ReflexiveDict object, dictionary with all cards and
+#                             cardstorm_ids pointing to each other
+#
+#         OUTPUT:
+#             - deck_dict: dictionary, dict with cardstorm ids as keys and lowercase
+#                             card counts as values
+#     '''
+#     deck_dict = {}
+#     for card_string in deck_list.split('\n'):
+#         if card_string.lower().startswith('sideboard'):
+#             # we found the sideboard, ignore everything past this line
+#             break
+#         if not card_string:
+#             # we found an empty line, ignore everything past this line
+#             break
+#         parse_status, card_name, card_count = parse_card_string(card_string)
+#         try:
+#             cardstorm_id = card_dict[card_name.lower()]
+#         except KeyError:
+#             print('card "{}" not found'.format(card_name))
+#         deck_dict[cardstorm_id] = int(card_count)
+#
+#     return deck_dict
+#
+# def vectorize_deck(all_cardstorm_ids, deck_dict):
+#     '''
+#         Creates an (n x 1) vector, where n is the number of available cards
+#
+#         INPUT:
+#             - all_cardstorm_ids: numpy array of all the available cardstorm_ids
+#             - deck_dict: dict representation of user submitted deck list. keys
+#                             are cardstorm_ids and values are card counts
+#         OUTPUT:
+#             - deck_vector: numpy array of shape (n x 1), where n is the number of
+#                             cardstorm_ids in all_cardstorm_ids. Each entry corresponds
+#                             to the corresponding cardstorm_id in all_cardstorm_ids,
+#                             and it is either the count of the card in the deck or 0
+#                             if it's not included in the deck.
+#     '''
+#
+#     deck_vector = []
+#
+#     for cardstorm_id in all_cardstorm_ids:
+#         if cardstorm_id in deck_dict:
+#             deck_vector.append(deck_dict[cardstorm_id])
+#         else:
+#             deck_vector.append(0)
+#
+#     deck_vector = np.array(deck_vector)
+#
+#     return deck_vector
+#
+# def get_feature_matrix():
+#     '''
+#         Pulls the most recent feature matrix from the databse and formats it properly
+#
+#         INPUT:
+#             NONE
+#         OUTPUT:
+#             - feature_matrix: numpy array of size (n x k), where n is the number of
+#                                 cards and k is chosen in the modeling process
+#     '''
+#     global cursor
+#     query = '''SELECT cardstorm_id, features
+#                FROM product_matrices
+#                WHERE date = (SELECT MAX(date) FROM product_matrices)
+#                ORDER BY cardstorm_id ASC'''
+#     cursor.execute(query)
+#
+#     feature_matrix = []
+#
+#     for cardstorm_id, features in cursor.fetchall():
+#         feature_matrix.append(features)
+#
+#     feature_matrix = np.array(feature_matrix)
+#
+#     return feature_matrix
+#
+# def solve_system(feature_matrix, deck_vector):
+#     '''
+#         Gets the least squares solution to the problem.
+#
+#         INPUT:
+#             - feature_matrix: numpy array of shape (n x k), where n is the number of
+#                                 cards and k is chosen in the modeling process
+#             - deck_vector: numpy array of shape (n x 1), where n is the number of
+#                             cards
+#
+#         OUTPUT:
+#             - u_vector: numpy array, the least squares solution to the system
+#                             deck_vector = (u_vector)*(feature_matrix)
+#     '''
+#
+#     if len(deck_vector) > 0:
+#         u_vector = np.linalg.lstsq(feature_matrix, deck_vector)[0]
+#     else:
+#         return np.array([])
+#
+#     return u_vector
+#
+# def top_n_recommendations(recommendations, deck_vector, all_cardstorm_ids, n=10):
+#     '''
+#         Gets the top n recommendations from the recreated deck list
+#
+#         INPUT:
+#             - recommendations: numpy array, the recreated deck_vector with predictions filled in
+#             - deck_vector: numpy array, the original deck_vector
+#             - n: int, the number of recommendations desired. default 10
+#
+#         OUTPUT:
+#             top_recommendations: numpy array, the top n recommendations in descending order
+#     '''
+#     top_recommendations = all_cardstorm_ids[np.argsort(recommendations - deck_vector)[:-n-1:-1]]
+#
+#     return top_recommendations
+#
+# def make_recommendations(raw_deck_list, num_top_recommendations=10):
+#     '''
+#
+#         get all cardstorm ids
+#         read user submitted deck file
+#         make deck dictionary
+#         make deck vector
+#         get feature matrix from db
+#         solve for u
+#         get recommendations
+#
+#     '''
+#     card_dict = ReflexiveDict()
+#     all_cardstorm_ids = card_dict.get_cardstorm_ids()
+#
+#     deck_dict = deck_to_dict(raw_deck_list, card_dict)
+#
+#     deck_vector = vectorize_deck(all_cardstorm_ids, deck_dict)
+#
+#     feature_matrix = get_feature_matrix()
+#
+#     u_vector = solve_system(feature_matrix, deck_vector)
+#
+#     if len(u_vector) == 0: # something went wrong
+#         print('deck vector was empty')
+#         return
+#
+#     recommendations = np.dot(u_vector, feature_matrix.T)
+#
+#     top_recommendations = top_n_recommendations(recommendations, deck_vector, all_cardstorm_ids, n=num_top_recommendations)
+#
+#     return top_recommendations
