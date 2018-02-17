354f0b4a84301a0554b47b020aa166dce2ace211
Ben Walzer <bwalzer@me.com>
2018-02-17 11:17:55 -0800

    updated path for python3 in script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,10 @@
 # and creates a new model
 
 # get new cards from scryfall
-python3 /home/ubuntu/cardstorm/src/card_scraping.py 1>>/home/ubuntu/cardstorm_logs/card_stdout.log 2>>/home/ubuntu/cardstorm_logs/card_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/card_scraping.py 1>>/home/ubuntu/cardstorm_logs/card_stdout.log 2>>/home/ubuntu/cardstorm_logs/card_stderr.log
 
 # get new decks from mtgtop8.com
-python3 /home/ubuntu/cardstorm/src/deck_scraping.py 1>>/home/ubuntu/cardstorm_logs/deck_stdout.log 2>>/home/ubuntu/cardstorm_logs/deck_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/deck_scraping.py 1>>/home/ubuntu/cardstorm_logs/deck_stdout.log 2>>/home/ubuntu/cardstorm_logs/deck_stderr.log
 
 # creates a new model
-python3 /home/ubuntu/cardstorm/src/modeling.py 1>>/home/ubuntu/cardstorm_logs/model_stdout.log 2>>/home/ubuntu/cardstorm_logs/model_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/modeling.py 1>>/home/ubuntu/cardstorm_logs/model_stdout.log 2>>/home/ubuntu/cardstorm_logs/model_stderr.log
