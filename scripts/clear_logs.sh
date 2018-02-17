058a4f8e25f63bd990e87e9a756cd0ec6db8579e
Ben Walzer <bwalzer@me.com>
2018-02-17 11:14:12 -0800

    added script to clear logs

---
+++ scripts/clear_logs.sh
@@ -0,0 +1,8 @@
+> ~/cardstorm_logs/card_stderr.log
+> ~/cardstorm_logs/card_stdout.log
+
+> ~/cardstorm_logs/deck_stderr.log
+> ~/cardstorm_logs/deck_stdout.log
+
+> ~/cardstorm_logs/model_stderr.log
+> ~/cardstorm_logs/model_stdout.log
