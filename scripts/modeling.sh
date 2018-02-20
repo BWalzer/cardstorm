d35ba60a3fa6faf3d12d82bc3cee171f093380fa
Ben Walzer <bwalzer@me.com>
2018-02-20 12:44:24 -0800

    fixed line issue

--- scripts/modeling.sh
+++ scripts/modeling.sh
@@ -9,5 +9,5 @@ export PYSPARK_DRIVER_PYTHON_OPTS=
 --conf spark.sql.warehouse.dir="file:///tmp/spark-warehouse" \
 --packages com.databricks:spark-csv_2.11:1.5.0 \
 --packages com.amazonaws:aws-java-sdk-pom:1.10.34 \
---packages org.apache.hadoop:hadoop-aws:2.7.3
+--packages org.apache.hadoop:hadoop-aws:2.7.3 \
 /home/ubuntu/cardstorm/src/modeling.py 1>>/home/ubuntu/cardstorm_logs/model_stdout.log 2>>/home/ubuntu/cardstorm_logs/model_stderr.log
