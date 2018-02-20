af4077551151821345e1f6cb8ac19666d3c8bf9a
Ben Walzer <bwalzer@me.com>
2018-02-20 11:27:21 -0800

    added modeling.sh script to run the modeling.py pyspark file

---
+++ scripts/modeling.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export PYSPARK_DRIVER_PYTHON=
+export PYSPARK_DRIVER_PYTHON_OPTS=
+
+${SPARK_HOME}/bin/spark-submnit \
+--master local[4] \
+--executor-memory 1G \
+--driver-memory 1G \
+--conf spark.sql.warehouse.dir="file:///tmp/spark-warehouse" \
+--packages com.databricks:spark-csv_2.11:1.5.0 \
+--packages com.amazonaws:aws-java-sdk-pom:1.10.34 \
+--packages org.apache.hadoop:hadoop-aws:2.7.3
+/home/ubuntu/cardstorm/src/modeling.py 1>>/home/ubuntu/cardstorm_logs/model_stdout.log 2>>/home/ubuntu/cardstorm_logs/model_stderr.log
