13f1060f7d20e3ee8c024c211ced244927eaf57d
Ben Walzer <bwalzer@me.com>
2018-02-20 13:08:16 -0800

    fixed scripts to work with spark on AWS

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,10 @@
 # and creates a new model
 
 # get new cards from scryfall
-/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/card_scraping.py 1>>/home/ubuntu/cardstorm_logs/card_stdout.log 2>>/home/ubuntu/cardstorm_logs/card_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/card_scraping.py 1>/home/ubuntu/cardstorm_logs/card_stdout.log 2>/home/ubuntu/cardstorm_logs/card_stderr.log
 
 # get new decks from mtgtop8.com
-/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/deck_scraping.py 1>>/home/ubuntu/cardstorm_logs/deck_stdout.log 2>>/home/ubuntu/cardstorm_logs/deck_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/deck_scraping.py 1>/home/ubuntu/cardstorm_logs/deck_stdout.log 2>/home/ubuntu/cardstorm_logs/deck_stderr.log
 
 # creates a new model
-/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/modeling.py 1>>/home/ubuntu/cardstorm_logs/model_stdout.log 2>>/home/ubuntu/cardstorm_logs/model_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/scripts/modeling.sh
