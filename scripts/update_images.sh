d6a4e9cde759f0eaf8d71640ef1220c03a734b58
Ben Walzer <bwalzer@me.com>
2018-02-21 13:08:24 -0800

    changed file path in update_images.sh

--- scripts/update_images.sh
+++ scripts/update_images.sh
@@ -1,2 +1,2 @@
 # gets new images from scryfall
-/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/image_scraping 1>/home/ubuntu/cardstorm_logs/image_stdout.log 2>/home/ubuntu/cardstorm_logs/image_stderr.log
+/home/ubuntu/anaconda3/bin/python3 /home/ubuntu/cardstorm/src/image_scraping.py 1>/home/ubuntu/cardstorm_logs/image_stdout.log 2>/home/ubuntu/cardstorm_logs/image_stderr.log
